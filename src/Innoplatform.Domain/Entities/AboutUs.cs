5313f037b74212e0825d420212dea06772586d6d
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2023-12-22 22:51:42 +0500

    added new entites and old entites's dtos#

--- src/Innoplatform.Domain/Entities/AboutUs.cs
+++ src/Innoplatform.Domain/Entities/AboutUs.cs
@@ -1,13 +1,8 @@
 ﻿using Innoplatform.Domain.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Innoplatform.Domain.Entities
 {
-    public class AboutUs:Auditable
+    public class AboutUs : Auditable
     {
         public string Title { get; set; }
         public string Description { get; set; }
--- src/Innoplatform.Service/DTOs/RecommendationAreas/RecommondationAreaForUpdateDto.cs
+++ src/Innoplatform.Service/DTOs/RecommendationAreas/RecommondationAreaForUpdateDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Innoplatform.Service.DTOs.RecommendationAreas
+﻿namespace Innoplatform.Service.DTOs.RecommendationAreas
 {
     public class RecommondationAreaForUpdateDto
     {
