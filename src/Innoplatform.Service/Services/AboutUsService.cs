d7d1618afca7373cc89e6abdbc7c257af7a6d3fb
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2023-12-23 03:17:43 +0500

    services implemented

--- src/Innoplatform.Service/Services/AboutUsService.cs
+++ src/Innoplatform.Service/Services/AboutUsService.cs
@@ -1,61 +1,87 @@
 ﻿using AutoMapper;
 using Innoplatform.Data.IRepositories;
-using Innoplatform.Domain.Entities;
 using Innoplatform.Domain.Entities.About;
-using Innoplatform.Service.DTOs.AboutUsAssets;
 using Innoplatform.Service.DTOs.AboutUses;
 using Innoplatform.Service.Exceptions;
 using Innoplatform.Service.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Innoplatform.Service.Services
+namespace Innoplatform.Service.Services;
+
+public class AboutUsService : IAboutUsService
 {
-    public class AboutUsService : IAboutUsService
+    private readonly IMapper _mapper;
+    private readonly IRepository<AboutUs> _repository;
+
+    public AboutUsService(IRepository<AboutUs> repository, IMapper mapper)
     {
-        private readonly IMapper _mapper;
-        private readonly IRepository<AboutUs> _repository;
+        _repository = repository;
+        _mapper = mapper;
+    }
 
-        public AboutUsService(IRepository<AboutUs> repository, IMapper mapper)
-        {
-            _repository = repository;
-            _mapper = mapper;
-        }
+    public async Task<AboutUsResultDto> AddAsync(AboutUsForCreationDto dto)
+    {
+        var aboutUs = await _repository.SelectAll()
+            .Where(au => au.Title == dto.Title)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
 
-        public async Task<AboutUsResultDto> AddAsync(AboutUsForCreationDto dto)
-        {
-            var aboutUs = await _repository.SelectAll()
-                .Where( au => au.Title == dto.Title )
-                .AsNoTracking()
-                .FirstOrDefaultAsync();
+        if (aboutUs is not null)
             throw new InnoplatformException(404, "aboutUs is already exist");
 
-            var mappedAboutUs = _mapper.Map<AboutUs>(dto);
-            return _mapper.Map<AboutUsResultDto>(await _repository.CreateAsync(mappedAboutUs));
-        }
+        var mappedAboutUs = _mapper.Map<AboutUs>(dto);
+        return _mapper.Map<AboutUsResultDto>(await _repository.CreateAsync(mappedAboutUs));
+    }
 
-        public Task<IEnumerable<AboutUsResultDto>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<IEnumerable<AboutUsResultDto>> GetAllAsync()
+    {
+        var aboutUsList = await _repository.SelectAll().ToListAsync();
 
-        public Task<AboutUsResultDto> GetByIdAsync(long id)
-        {
-            throw new NotImplementedException();
-        }
+        return _mapper.Map<IEnumerable<AboutUsResultDto>>(aboutUsList);
+    }
 
-        public Task<AboutUsResultDto> ModifyAsync(long id, AboutUsForUpdateDto dto)
-        {
-            throw new NotImplementedException();
-        }
+    public async Task<AboutUsResultDto> GetByIdAsync(long id)
+    {
+        var aboutUs = await _repository.SelectAll()
+            .Where(au => au.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
 
-        public Task<bool> RemoveAsync(long id)
-        {
-            throw new NotImplementedException();
-        }
+        if (aboutUs is null)
+            throw new InnoplatformException(409, "aboutUs is not found in this Id");
+
+        var mappedAboutUs = _mapper.Map<AboutUsResultDto>(aboutUs);
+        return mappedAboutUs;
+    }
+
+    public async Task<AboutUsResultDto> ModifyAsync(long id, AboutUsForUpdateDto dto)
+    {
+        var aboutUs = await _repository.SelectAll()
+            .Where(au => au.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (aboutUs is null)
+            throw new InnoplatformException(409, "aboutUs is not found in this Id");
+
+        var mapped = _mapper.Map(dto, aboutUs);
+        mapped.UpdatedAt = DateTime.UtcNow;
+
+        var result = await _repository.UpdateAsync(mapped);
+
+        return _mapper.Map<AboutUsResultDto>(result);
+    }
+
+    public async Task<bool> RemoveAsync(long id)
+    {
+        var aboutUs = await _repository.SelectAll()
+            .Where(au => au.Id == id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (aboutUs is null)
+            throw new InnoplatformException(409, "aboutUs is not found in this Id");
+
+        return await _repository.DeleteAsync(id);
     }
 }
