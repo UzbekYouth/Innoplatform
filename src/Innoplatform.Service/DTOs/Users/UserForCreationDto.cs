ce49711a040637f2eaa58a0dd3167761c93a9a73
Sardorbek9343 <s.tolibjonov@tuit.uz>
2023-12-22 16:41:48 +0500

    EntitesAdded

--- src/Innoplatform.Service/DTOs/Users/UserForCreationDto.cs
+++ src/Innoplatform.Service/DTOs/Users/UserForCreationDto.cs
@@ -1,17 +1,16 @@
-﻿using Innoplatform.Domain.Commons;
+﻿using Microsoft.AspNetCore.Http;
 
-namespace Innoplatform.Domain.Entities;
+namespace Innoplatform.Service.DTOs.Users;
 
-public class User : Auditable
+public class UserForCreationDto
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string Image { get; set; }
+    public IFormFile Image { get; set; }
     public DateTime DateOfBirth { get; set; }
     public decimal AccauntBalance { get; set; }
     public string PhoneNumber { get; set; }
     public string Address { get; set; }
-    public string Hash { get; set; }
 }
