49f92f333a1fd090a593436d28bc2b33e6ee13f6
AzizbekFullStackDev <uzbreactdev@gmail.com>
2023-12-23 13:35:33 +0500

    Auth service is on process

--- src/Innoplatform.Service/Services/AuthServices/AuthService.cs
+++ src/Innoplatform.Service/Services/AuthServices/AuthService.cs
@@ -1,6 +1,34 @@
-﻿namespace Innoplatform.Service.Services.AuthServices
+﻿using Innoplatform.Data.IRepositories;
+using Innoplatform.Domain.Entities.Organizations;
+using Innoplatform.Domain.Entities.Users;
+using Innoplatform.Service.DTOs.Login;
+using Innoplatform.Service.Interfaces.IAuthServices;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System.Text;
+
+namespace Innoplatform.Service.Services.AuthServices
 {
-    internal class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IRepository<User> _userRepository;
+        private readonly IRepository<Organization> _organizationRepository;
+
+        public AuthService(IConfiguration configuration, IRepository<User> userRepository, IRepository<Organization> organizationRepository)
+        {
+            _configuration = configuration;
+            _userRepository = userRepository;
+            _organizationRepository = organizationRepository;
+        }
+
+        public Task<LoginForResultDto> AuthenticateAsync(LoginForCreationDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+       
     }
 }
