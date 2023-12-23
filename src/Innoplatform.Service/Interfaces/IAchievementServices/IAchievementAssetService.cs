05ae4c11ce772fe55608273c2eb0a0e17d8a1bd9
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2023-12-23 14:11:25 +0500

    mapping profile added all DTOs

--- src/Innoplatform.Service/Interfaces/IAchievementServices/IAchievementAssetService.cs
+++ src/Innoplatform.Service/Interfaces/IAchievementServices/IAchievementAssetService.cs
@@ -1,19 +1,12 @@
-﻿using Innoplatform.Service.DTOs.AboutUses;
-using Innoplatform.Service.DTOs.AchievementAssets;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Innoplatform.Service.DTOs.AchievementAssets;
 
-namespace Innoplatform.Service.Interfaces.IAchievmentServices
+namespace Innoplatform.Service.Interfaces.IAchievmentServices;
+
+public interface IAchievementAssetService
 {
-    public interface IAchievmentAssetService
-    {
-        Task<bool> RemoveAsync(long id);
-        Task<AchievementAssetsForResultDto> GetByIdAsync(long id);
-        Task<IEnumerable<AchievementAssetsForResultDto>> GetAllAsync();
-        Task<AchievementAssetsForResultDto> AddAsync(AchievmentAssetsForCreationDto dto);
-        Task<AchievementAssetsForResultDto> ModifyAsync(long id, AchievementAssetsForUpdateDto dto);
-    }
+    Task<bool> RemoveAsync(long id);
+    Task<AchievementAssetsForResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<AchievementAssetsForResultDto>> GetAllAsync();
+    Task<AchievementAssetsForResultDto> AddAsync(AchievmentAssetsForCreationDto dto);
+    Task<AchievementAssetsForResultDto> ModifyAsync(long id, AchievementAssetsForUpdateDto dto);
 }
