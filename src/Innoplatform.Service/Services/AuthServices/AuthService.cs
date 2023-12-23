18b55cc517618c20deeac9b3c98ed515007895d5
Sardorbek9343 <s.tolibjonov@tuit.uz>
2023-12-23 14:18:12 +0500

    Auth service has been completed

--- src/Innoplatform.Service/Services/AuthServices/AuthService.cs
+++ src/Innoplatform.Service/Services/AuthServices/AuthService.cs
@@ -1,14 +1,22 @@
-﻿using Innoplatform.Data.IRepositories;
+﻿using AutoMapper;
+using Innoplatform.Data.IRepositories;
 using Innoplatform.Domain.Entities.Organizations;
 using Innoplatform.Domain.Entities.Users;
 using Innoplatform.Service.DTOs.Login;
+using Innoplatform.Service.Exceptions;
+using Innoplatform.Service.Extensions;
 using Innoplatform.Service.Interfaces.IAuthServices;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using System.Xml;
 
 namespace Innoplatform.Service.Services.AuthServices
 {
+    // optimiza authrentication
     public class AuthService : IAuthService
     {
         private readonly IConfiguration _configuration;
@@ -22,13 +30,97 @@ namespace Innoplatform.Service.Services.AuthServices
             _organizationRepository = organizationRepository;
         }
 
-        public Task<LoginForResultDto> AuthenticateAsync(LoginForCreationDto dto)
+        public async Task<LoginForResultDto> AuthenticateAsync(LoginForCreationDto dto)
         {
-            throw new NotImplementedException();
+            var user = await _userRepository.SelectAll().Where(x => x.PhoneNumber == dto.PhoneNumber && x.IsDeleted == false)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            
+            var organization = await _organizationRepository.SelectAll().Where(x => x.PhoneNumber == dto.PhoneNumber && x.IsDeleted == false)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+            // write other logic here
+            if (user == null)
+            {
+                throw new InnoplatformException(400, "User not found");
+            }   
+            if(user != null)
+            {
+                var hashedPassword = PasswordHelper.Verify(dto.Password, user.Salt, user.Password);
+                if(hashedPassword == false)
+                {
+                    throw new InnoplatformException(400, "PhoneNumber or Password is incorrect");
+                }
+                return new LoginForResultDto
+                {
+                    Role = user.Role.ToString(),
+                    Token = GenerateToken(user),
+                };
+
+            }
+            else if(organization != null)
+            {
+                var hashedPassword = PasswordHelper.Verify(dto.Password, organization.Salt, organization.Password);
+                if(hashedPassword == false)
+                {
+                    throw new InnoplatformException(400, "PhoneNumber or Password is incorrect");
+                }   
+                return new LoginForResultDto
+                {
+                    Role = organization.Role.ToString(),
+                    Token = GenerateOrganizationToken(organization),
+                };
+            }
+            throw new InnoplatformException(400, "PhoneNumber or Password is incorrect");
+     
+        }
+
+        private string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+            new Claim("Id", user.Id.ToString()),
+            new Claim(ClaimTypes.Role, user.Role.ToString()),  // Use the role passed as an argument
+                }),
+                Audience = _configuration["JWT:Audience"],
+                Issuer = _configuration["JWT:Issuer"],
+                IssuedAt = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(_configuration["JWT:Expire"])),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private string GenerateOrganizationToken(Organization organization)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(_configuration["JWT:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("Id", organization.Id.ToString()),
+                    new Claim(ClaimTypes.Role, organization.Role.ToString()),  // Use the role passed as an argument
+                }),
+                Audience = _configuration["JWT:Audience"],
+                Issuer = _configuration["JWT:Issuer"],
+                IssuedAt = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(_configuration["JWT:Expire"])),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
 
 
-
-       
     }
 }
