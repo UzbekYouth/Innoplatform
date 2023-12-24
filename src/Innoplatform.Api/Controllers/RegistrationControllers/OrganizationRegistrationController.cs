cf05122a3f1011c7eb9a6a69e84b13df35e1d55d
AzizbekFullStackDev <uzbreactdev@gmail.com>
2023-12-24 07:01:05 +0500

    Changes are made in reg part

---
+++ src/Innoplatform.Api/Controllers/RegistrationControllers/OrganizationRegistrationController.cs
@@ -0,0 +1,55 @@
+﻿using Innoplatform.Api.Models;
+using Innoplatform.Service.DTOs.Registrations.UserRegistrations;
+using Innoplatform.Service.DTOs.Registrations;
+using Microsoft.AspNetCore.Mvc;
+using Innoplatform.Service.Interfaces.IRegistrationServices;
+using Innoplatform.Service.DTOs.Registrations.OrganizationRegistrations;
+
+namespace Innoplatform.Api.Controllers.RegistrationControllers
+{
+    public class OrganizationRegistrationController : BaseController
+    {
+        private readonly IOrganizationRegistrationService _organizationRegistrationService;
+        public OrganizationRegistrationController(IOrganizationRegistrationService organizationRegistrationService)
+        {
+            _organizationRegistrationService = organizationRegistrationService;
+        }
+
+        [HttpPost("Register")]
+
+        public async Task<IActionResult> PostAsync([FromBody] OrganizationRegistrationForCreationDto dto)
+        {
+            var response = new Response()
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await this._organizationRegistrationService.AddAsync(dto)
+            };
+            return Ok(response);
+        }
+
+        [HttpPost("SendVerificationCode")]
+        public async Task<IActionResult> SendVerificationCode([FromBody] SendVerificationCode dto)
+        {
+            var response = new Response()
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await this._organizationRegistrationService.SendVerificationCodeAsync(dto)
+            };
+            return Ok(response);
+        }
+
+        [HttpPost("VerifyCode")]
+        public async Task<IActionResult> VerifyCode([FromBody] CodeVerification dto)
+        {
+            var response = new Response()
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await this._organizationRegistrationService.VerifyCodeAsync(dto)
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ src/Innoplatform.Service/Interfaces/IRegistrationServices/IOrganizationRegistrationService.cs
@@ -0,0 +1,12 @@
+﻿using Innoplatform.Service.DTOs.Registrations;
+using Innoplatform.Service.DTOs.Registrations.OrganizationRegistrations;
+
+namespace Innoplatform.Service.Interfaces.IRegistrationServices
+{
+    public interface IOrganizationRegistrationService
+    {
+        public Task<OrganizationRegistrationForResultDto> AddAsync(OrganizationRegistrationForCreationDto dto);
+        public Task<string> SendVerificationCodeAsync(SendVerificationCode dto);
+        public Task<bool> VerifyCodeAsync(CodeVerification dto);
+    }
+}
